72f23cb97dee926fadb9d5c9523b2111d45dafa3
htchoi1995 <htchoi1995@gmail.com>
2016-01-03 20:26:36 +0100

    FWB: Disable/Enable screenshot sound [1/2]
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3637,6 +3637,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
