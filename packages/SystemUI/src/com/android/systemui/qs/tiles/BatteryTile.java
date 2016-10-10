e215c92f165de81f2839aa739d731ce12f1002cd
t-rex600 <fisch890@gmail.com>
2016-10-10 20:53:48 +0200

    Revert "QS: long press battery tile to toggle cpu info overlay"
    This reverts commit 28da0a3d70ff3b4a79d52b5411fa9a0235fdaaf1.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BatteryTile.java
@@ -43,9 +43,6 @@ import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
 import com.android.systemui.statusbar.policy.BatteryController;
 
-import android.provider.Settings;
-import android.provider.Settings.Global;
-
 import java.text.NumberFormat;
 
 public class BatteryTile extends QSTile<QSTile.State> implements BatteryController.BatteryStateChangeCallback {
@@ -97,13 +94,8 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
     }
 
     @Override
-    public void handleLongClick() {
-        setCpuInfoEnabled();
-    }
-
-    @Override
     public Intent getLongClickIntent() {
-        return null;
+        return new Intent(Intent.ACTION_POWER_USAGE_SUMMARY);
     }
 
     @Override
@@ -306,23 +298,5 @@ public class BatteryTile extends QSTile<QSTile.State> implements BatteryControll
                 postBindView();
             }
         };
-    }
-
-    private boolean setCpuInfoEnabled() {
-        boolean enabled = Settings.Global.getInt(mContext.getContentResolver(),
-                Settings.Global.SHOW_CPU, 1) != 0;
-        Intent service = (new Intent())
-                .setClassName("com.android.systemui",
-                "com.android.systemui.CPUInfoService");
-        if (!enabled) {
-            Settings.Global.putInt(
-                mContext.getContentResolver(), Settings.Global.SHOW_CPU, 1);
-            mContext.startService(service);
-        } else {
-            Settings.Global.putInt(
-                mContext.getContentResolver(), Settings.Global.SHOW_CPU, 0);
-            mContext.stopService(service);
-        }
-        return enabled;
     }
 }
